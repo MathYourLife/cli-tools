792157044aec57467839eaad97cde28d62113bbc
Dan Couture <MathYourLife@gmail.com>
2016-11-12 00:30:49 -0500

    Adding histogram printout

--- histogram/main.go
+++ histogram/main.go
@@ -13,13 +13,23 @@ import (
 	"math"
 	"io"
 	"os"
+	"sort"
 	"strconv"
+	"strings"
 )
+
+type Data []float64
+
+func (d Data) Len() int           { return len(d) }
+func (d Data) Swap(i, j int)      { d[i], d[j] = d[j], d[i] }
+func (d Data) Less(i, j int) bool { return d[i] < d[j] }
 
 type Histogram struct {
 	input chan string
 	stopInput chan bool
 	inputComplete chan bool
+
+	data Data
 
 	max float64
 	min float64
@@ -32,6 +42,8 @@ func NewHistogram() (*Histogram, error) {
 	h := &Histogram{
 		min: math.MaxFloat64,
 		max: -math.MaxFloat64,
+
+		data: Data{},
 
 		input: make(chan string),
 		stopInput: make(chan bool),
@@ -63,6 +75,7 @@ PARSE_LOOP:
 }
 
 func (h *Histogram) addValue(v float64) {
+	h.data = append(h.data, v)
 	if h.count == 0 {
 		h.ss = 0
 	} else {
@@ -95,6 +108,44 @@ func (h *Histogram) Close() {
 	fmt.Printf("ss:\t%f\n", h.ss)
 	fmt.Printf("var:\t%f\n", h.ss / h.count)
 	fmt.Printf("sd:\t%f\n", math.Sqrt(h.ss / h.count))
+
+	sort.Sort(h.data)
+	bins := float64(10)
+	w := (h.max - h.min) / bins
+	idx := 0
+	boundaries := []int{0}
+	for bin := 0.0; bin < bins; bin++ {
+		for {
+			if h.data[idx] >= w * (bin + 1) + h.min {
+				boundaries = append(boundaries, idx)
+				break
+			}
+			idx++
+		}
+	}
+	y := 0
+	labels := []string{}
+	repeats := []int{}
+	maxY := 0
+	for bin := 1; bin < int(bins) + 1; bin++ {
+		if bin == int(bins) {
+			y = boundaries[bin] - boundaries[bin - 1] + 1
+		} else {
+			y = boundaries[bin] - boundaries[bin - 1]
+		}
+		labels = append(labels, fmt.Sprintf("%10.4f - %10.4f [%6d]: ", h.min + (w * float64(bin - 1)), h.min + (w * float64(bin)), y))
+		repeats = append(repeats, y)
+		if y > maxY {
+			maxY = y
+		}
+	}
+
+	scale := maxY / 75
+	fmt.Printf("∎:\t%d\n", scale)
+	for i := 0; i < len(labels); i++ {
+		fmt.Printf(labels[i])
+		fmt.Println(strings.Repeat("∎", repeats[i] / scale))
+	}
 }
 
 func main() {
